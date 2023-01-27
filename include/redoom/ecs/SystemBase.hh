adrienlucbert/redoom

commit ff3aa12a491dfe9e38846d3a21e06e692676e307

feat: framebuffer and render pipeline support
feat: render scene to framebuffer before screen
feat: mouse picking system

examples/3d_scene/scenes/default.yaml
@@ -25,7 +25,6 @@ Entities:
       radius: 10
       x_segment: 20
       y_segment: 20
-      topology: 4
     MaterialComponent:
       shader: lit
       gloss: 6
@@ -42,13 +41,13 @@ Entities:
       radius: 10
       x_segment: 20
       y_segment: 20
-      topology: 4
     MaterialComponent:
       shader: lit
       gloss: 6
       smoothness: 1
       metallicness: 0.200000003
-      color: [0, 0, 0]
+      color: [1, 0, 1]
+      topology: 4
   - TransformComponent:
       position: [2, 0, 0]
       angle: 0
@@ -60,6 +59,7 @@ Entities:
       smoothness: 1
       metallicness: 0.100000001
       color: [1, 1, 1]
+      topology: 4
     ModelComponent:
       path: ../assets/backpack/backpack.obj
       flip_uvs: true

include/redoom/ecs/systems/RenderSystem.hh
@@ -4,7 +4,7 @@
 
 namespace redoom::ecs::systems
 {
-struct ModelSystem : public MonothreadedSystem<ModelSystem> {
+struct RenderSystem : public MonothreadedSystem<RenderSystem> {
   void update(UpdateContext& context) noexcept override;
 };
 } // namespace redoom::ecs::systems

src/ecs/systems/RenderSystem.cpp
@@ -1,0 +1,56 @@
+#include <redoom/ecs/systems/RenderSystem.hh>
+
+#include <Utils/Concepts.hpp>
+#include <redoom/ecs/components/MaterialComponent.hh>
+#include <redoom/ecs/components/MeshComponent.hh>
+#include <redoom/ecs/components/ModelComponent.hh>
+#include <redoom/ecs/components/TransformComponent.hh>
+#include <redoom/graphics/ShaderLibrary.hh>
+#include <redoom/renderer/Renderer.hh>
+
+namespace redoom::ecs::systems
+{
+namespace detail
+{
+template <concepts::Drawable T>
+static void renderDrawable(
+    UpdateContext& context, Entity entity, T const& drawable)
+{
+  auto model = glm::mat4(1.0f);
+  auto transform_opt =
+      context.getComponentManager().get<components::TransformComponent>(entity);
+  if (transform_opt) {
+    auto& transform = *transform_opt;
+    model = glm::translate(model, transform.getPosition());
+    model = glm::scale(model, transform.getScale());
+    model = glm::rotate(model, transform.getAngle(), transform.getRotation());
+  }
+  renderer::Renderer::get().setGlobalUniform("EntityId",
+      graphics::uniforms::Uint{.value = static_cast<GLuint>(entity)});
+  auto material_opt =
+      context.getComponentManager().get<components::MaterialComponent>(entity);
+  if (material_opt) {
+    (*material_opt).material.use();
+  } else {
+    renderer::Renderer::get().getRenderPipeline().getDefaultMaterial().use();
+  }
+  renderer::Renderer::get().draw(drawable, model);
+}
+} // namespace detail
+
+void RenderSystem::update(UpdateContext& context) noexcept
+{
+  renderer::Renderer::get().beginRendering();
+
+  context.getComponentManager().apply<components::MeshComponent>(
+      [&](auto entity, components::MeshComponent& component) {
+        detail::renderDrawable(context, entity, *component.mesh_);
+      });
+  context.getComponentManager().apply<components::ModelComponent>(
+      [&](auto entity, components::ModelComponent& component) {
+        detail::renderDrawable(context, entity, component.model_);
+      });
+
+  renderer::Renderer::get().endRendering();
+}
+} // namespace redoom::ecs::systems

commit ef05974da29250a182aac85581c47a172a75d57d

feat: add pre/post update hooks to systems
fix: re-enable physics debug draw (probably broke in ff3aa12a)

examples/3d_scene/scenes/default.yaml
@@ -13,7 +13,7 @@ Entities:
   - AmbientLightComponent:
       color: [0.100000001, 0.100000001, 0.100000001]
   - DirectionalLightComponent:
-      direction: [1, 1, 1]
+      direction: [-1, 1, 1]
       color: [1, 1, 1]
   - TransformComponent:
       position: [1.5, 0, 0]
@@ -31,6 +31,8 @@ Entities:
       smoothness: 1
       metallicness: 0.200000003
       color: [1, 0, 0]
+    BehaviourComponent:
+      RotationBehaviour
   - TransformComponent:
       position: [1, 0, 0]
       angle: 0
@@ -48,22 +50,26 @@ Entities:
       metallicness: 0.200000003
       color: [1, 0, 1]
       topology: 4
-  # - TransformComponent:
-  #     position: [2, 0, 0]
-  #     angle: 0
-  #     rotation: [0, 1, 0]
-  #     scale: [0.100000001, 0.100000001, 0.100000001]
-  #   MaterialComponent:
-  #     shader: lit
-  #     gloss: 6
-  #     smoothness: 1
-  #     metallicness: 0.100000001
-  #     color: [1, 1, 1]
-  #     topology: 4
-  #   ModelComponent:
-  #     path: ../assets/backpack/backpack.obj
-  #     flip_uvs: true
-  # - BehaviourComponent:
-  #     KeyboardBehaviour
-  # - BehaviourComponent:
-  #     FPSCounterBehaviour
+    BehaviourComponent:
+      RotationBehaviour
+  - TransformComponent:
+      position: [2, 0, 0]
+      angle: 0
+      rotation: [0, 1, 0]
+      scale: [0.100000001, 0.100000001, 0.100000001]
+    MaterialComponent:
+      shader: lit
+      gloss: 6
+      smoothness: 1
+      metallicness: 0.100000001
+      color: [1, 1, 1]
+      topology: 4
+    ModelComponent:
+      path: ../assets/backpack/backpack.obj
+      flip_uvs: true
+    BehaviourComponent:
+      RotationBehaviour
+  - BehaviourComponent:
+      KeyboardBehaviour
+  - BehaviourComponent:
+      FPSCounterBehaviour

include/redoom/ecs/SystemBase.hh
@@ -14,7 +14,9 @@ public:
   SystemBase& operator=(SystemBase const&) noexcept = default;
   SystemBase& operator=(SystemBase&&) noexcept = default;
 
+  virtual void beforeUpdate(UpdateContext& /*unused*/) noexcept;
   virtual void update(UpdateContext& /*unused*/) noexcept;
+  virtual void afterUpdate(UpdateContext& /*unused*/) noexcept;
 
   [[nodiscard]] bool isMultithreaded() const noexcept;
 

include/redoom/ecs/SystemManager.hh
@@ -86,6 +86,9 @@ public:
   void update(UpdateContext& context) noexcept;
 
 private:
+  void runUpdateHook(void (SystemBase::*hook)(UpdateContext&),
+      UpdateContext& context) noexcept;
+
   struct SystemData {
     Utils::type_id type_id;
     std::unique_ptr<SystemBase> system;

include/redoom/ecs/systems/RenderSystem.hh
@@ -5,6 +5,8 @@
 namespace redoom::ecs::systems
 {
 struct RenderSystem : public MonothreadedSystem<RenderSystem> {
+  void beforeUpdate(UpdateContext& context) noexcept override;
   void update(UpdateContext& context) noexcept override;
+  void afterUpdate(UpdateContext& context) noexcept override;
 };
 } // namespace redoom::ecs::systems

src/ecs/SystemBase.cpp
@@ -7,7 +7,15 @@ SystemBase::SystemBase(bool pis_multithreaded) noexcept
 {
 }
 
+void SystemBase::beforeUpdate(UpdateContext& /*unused*/) noexcept
+{
+}
+
 void SystemBase::update(UpdateContext& /*unused*/) noexcept
+{
+}
+
+void SystemBase::afterUpdate(UpdateContext& /*unused*/) noexcept
 {
 }
 

src/ecs/SystemManager.cpp
@@ -4,7 +4,8 @@
 
 namespace redoom::ecs
 {
-void SystemManager::update(UpdateContext& context) noexcept
+void SystemManager::runUpdateHook(
+    void (SystemBase::*hook)(UpdateContext&), UpdateContext& context) noexcept
 {
   auto futures = std::vector<std::future<void>>{};
 
@@ -12,15 +13,22 @@ void SystemManager::update(UpdateContext& context) noexcept
     auto& system = pair.second.system;
     if (system->isMultithreaded()) {
       auto future =
-          this->thread_pool_.enqueue([&] { system->update(context); });
+          this->thread_pool_.enqueue([&] { (*system.*hook)(context); });
       futures.emplace_back(std::move(future));
     } else {
-      system->update(context);
+      (*system.*hook)(context);
     }
   }
 
   for (auto& future : futures) {
     future.wait();
   }
+}
+
+void SystemManager::update(UpdateContext& context) noexcept
+{
+  this->runUpdateHook(&SystemBase::beforeUpdate, context);
+  this->runUpdateHook(&SystemBase::update, context);
+  this->runUpdateHook(&SystemBase::afterUpdate, context);
 }
 } // namespace redoom::ecs

src/ecs/systems/RenderSystem.cpp
@@ -38,10 +38,13 @@ static void renderDrawable(
 }
 } // namespace detail
 
-void RenderSystem::update(UpdateContext& context) noexcept
+void RenderSystem::beforeUpdate(UpdateContext& /*context*/) noexcept
 {
   renderer::Renderer::get().beginRendering();
+}
 
+void RenderSystem::update(UpdateContext& context) noexcept
+{
   context.getComponentManager().apply<components::MeshComponent>(
       [&](auto entity, components::MeshComponent& component) {
         detail::renderDrawable(context, entity, *component.mesh_);
@@ -50,7 +53,10 @@ void RenderSystem::update(UpdateContext& context) noexcept
       [&](auto entity, components::ModelComponent& component) {
         detail::renderDrawable(context, entity, component.model_);
       });
+}
 
+void RenderSystem::afterUpdate(UpdateContext& /*context*/) noexcept
+{
   renderer::Renderer::get().endRendering();
 }
 } // namespace redoom::ecs::systems